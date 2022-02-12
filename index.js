d067acdce1c88219edc8798f2fe9fff206a69bf3
Uros <urosivanovich@gmail.com>
2022-02-12 18:12:35 -0500

    wip

---
+++ index.js
@@ -0,0 +1,32 @@
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+
+const server = express()
+
+const PORT = process.env.PORT || 9000
+
+server.use(express.json())
+server.use(cors())
+
+server.get('/api/hello', (req, res) => {
+    res.json({
+        message: 'API is working'
+    })
+})
+
+server.use('*', (req, res) => {
+    res.send(`<h1>Hello there!</h1>`)
+})
+
+
+server.use((err, req, res, next) => { // eslint-disable-line
+    res.status(500).json({
+        message: err.message,
+        stack: err.stack
+    })
+}) 
+
+server.listen(PORT, () => {
+    console.log(`listening on ${PORT}`)
+})
